e93d7cb02921528a4ba384d86699f2a83ff1a14e
Wang Yuchun <wyuch@zving.com>
2017-10-19 21:55:53 +0800

    完善JSON存储功能

--- common/src/main/java/com/zving/declarationform/storage/IStorage.java
+++ common/src/main/java/com/zving/declarationform/storage/IStorage.java
@@ -1,5 +1,7 @@
 package com.zving.declarationform.storage;
 
+import java.util.List;
+
 /**
  * 数据存取
  * 
@@ -8,13 +10,28 @@ package com.zving.declarationform.storage;
  * @date 2017年10月19日
  */
 public interface IStorage {
+	/**
+	 * 返回与bean中非null字段的值一致的第一个对象
+	 */
 	<T> T get(Class<T> clazz, T bean);
 
-	<T> T[] find(Class<T> clazz, T bean);
+	/**
+	 * 返回与bean中非null字段的值一致的所有对象
+	 */
+	<T> List<T> find(Class<T> clazz, T bean);
 
-	<T> T add(Class<T> clazz, T bean);
+	/**
+	 * 加入一个对象
+	 */
+	<T> void add(Class<T> clazz, T bean);
 
-	<T> T update(Class<T> clazz, T bean);
+	/**
+	 * 更新一个对象
+	 */
+	<T> void update(Class<T> clazz, T bean);
 
-	<T> T delete(Class<T> clazz, T bean);
+	/**
+	 * 删除一个对象
+	 */
+	<T> void delete(Class<T> clazz, T bean);
 }
--- common/src/main/java/com/zving/declarationform/storage/JSONStorage.java
+++ common/src/main/java/com/zving/declarationform/storage/JSONStorage.java
@@ -1,35 +1,125 @@
 package com.zving.declarationform.storage;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Collectors;
+
+import org.apache.commons.beanutils.BeanMap;
+import org.apache.commons.beanutils.BeanUtils;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
+ * 将数据存储成JSON，开发时临时使用
+ * 
  * @author 王育春
  * @mail wyuch@zving.com
  * @date 2017年10月19日
  */
 public class JSONStorage implements IStorage {
+	ConcurrentHashMap<Class<?>, List<?>> map;
+	ReentrantLock lock = new ReentrantLock();
+
+	@SuppressWarnings("unchecked")
+	<T> List<T> getList(Class<T> clazz) {
+		if (map == null) {
+			lock.lock();
+			try {
+				if (map == null) {
+				}
+			} finally {
+				lock.unlock();
+			}
+		}
+
+		if (map.contains(clazz)) {
+			return (List<T>) map.get(clazz);
+		} else {
+			lock.lock();
+			try {
+				if (!map.contains(clazz)) {
+					ObjectMapper om = new ObjectMapper();
+					JavaType type = om.getTypeFactory().constructParametricType(ArrayList.class, clazz);
+					try {
+						List<T> list = om.readValue(new File("appdata/json/" + clazz.getName()), type);
+						map.put(clazz, list);
+						return list;
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			} finally {
+				lock.unlock();
+			}
+		}
+		List<T> list = new ArrayList<T>();
+		map.put(clazz, list);
+		return list;
+	}
+
+	<T> void write(Class<T> clazz) {
+		List<T> list = getList(clazz);
+		ObjectMapper om = new ObjectMapper();
+		try {
+			om.writeValue(new File("appdata/json/" + clazz.getName()), list);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Override
 	public <T> T get(Class<T> clazz, T bean) {
+		List<T> arr = find(clazz, bean);
+		if (arr != null && arr.size() > 0) {
+			return arr.get(0);
+		}
 		return null;
 	}
 
 	@Override
-	public <T> T[] find(Class<T> clazz, T bean) {
-		return null;
+	public <T> List<T> find(Class<T> clazz, T bean) {
+		List<T> list = getList(clazz);
+		BeanMap map1 = new BeanMap(bean);
+		bean.getClass().getDeclaredMethods();
+		return list.stream().filter(item -> {
+			BeanMap map2 = new BeanMap(item);
+			for (Entry<?, ?> e : map1.entrySet()) {
+				if (e.getValue() != null && e.getValue() != map2.get(e.getKey())) {
+					return false;
+				}
+			}
+			return true;
+		}).collect(Collectors.toList());
 	}
 
 	@Override
-	public <T> T add(Class<T> clazz, T bean) {
-		return null;
+	public <T> void add(Class<T> clazz, T bean) {
+		List<T> list = getList(clazz);
+		list.add(bean);
+		write(clazz);
 	}
 
 	@Override
-	public <T> T update(Class<T> clazz, T bean) {
-		return null;
+	public <T> void update(Class<T> clazz, T bean) {
+		T dest = get(clazz, bean);
+		try {
+			BeanUtils.copyProperties(bean, dest);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		write(clazz);
 	}
 
 	@Override
-	public <T> T delete(Class<T> clazz, T bean) {
-		return null;
+	public <T> void delete(Class<T> clazz, T bean) {
+		getList(clazz).remove(bean);
+		write(clazz);
 	}
 
 }
