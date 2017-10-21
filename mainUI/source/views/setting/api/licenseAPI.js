0391d61d3c7b6bef5f337ded185005069fad065b
zp <zp@zving.com>
2017-10-21 18:27:14 +0800

    修改licenseAPI.js更新方法

--- mainUI/source/views/setting/api/licenseAPI.js
+++ mainUI/source/views/setting/api/licenseAPI.js
@@ -14,11 +14,9 @@ const licenseAPI = {
   },
 
   updateLicense (license) {
-    return axios({
-      method: 'put',
-      url: '/api/license/' + license.id,
-      data: license
-    }).then(res => res.data)
+    return axios
+      .put('/api/license', license)
+      .then(res => res.data)
   },
 
   getOutlicenseList (search) {
