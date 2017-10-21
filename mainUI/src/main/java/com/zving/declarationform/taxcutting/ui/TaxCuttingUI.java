f84326cba963fad78c133501cf5c63bbb639dbfc
lwb <lwb@zving.com>
2017-10-21 22:05:30 +0800

    注释编译错误代码

--- mainUI/src/main/java/com/zving/declarationform/taxcutting/ui/TaxCuttingUI.java
+++ mainUI/src/main/java/com/zving/declarationform/taxcutting/ui/TaxCuttingUI.java
@@ -13,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.zving.declarationform.base.BaseUI;
 import com.zving.declarationform.dto.ResponseDTO;
-import com.zving.declarationform.model.TaxCuttingCode;
+//import com.zving.declarationform.model.TaxCuttingCode;
 import com.zving.declarationform.model.TaxCuttingRule;
 
 import io.servicecomb.provider.rest.common.RestSchema;
@@ -21,64 +21,64 @@ import io.servicecomb.provider.springmvc.reference.RestTemplateBuilder;
 
 @RestSchema(schemaId = "taxCutting")
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON)
-public class TaxCuttingUI extends BaseUI{
-	@RequestMapping(path = "taxcutting", method = RequestMethod.POST)
-	public @ResponseBody ResponseDTO addTaxCutting(@RequestBody TaxCuttingRule taxCuttingRule) {
-		RestTemplate restTemplate = RestTemplateBuilder.create();
-		restTemplate.postForObject("cse://taxCutting/taxcutting", taxCuttingRule, String.class);
-		return success("添加成功");
-	}
+public class TaxCuttingUI extends BaseUI {
+    @RequestMapping(path = "taxcutting", method = RequestMethod.POST)
+    public @ResponseBody ResponseDTO addTaxCutting(@RequestBody TaxCuttingRule taxCuttingRule) {
+        RestTemplate restTemplate = RestTemplateBuilder.create();
+        restTemplate.postForObject("cse://taxCutting/taxcutting", taxCuttingRule, String.class);
+        return success("添加成功");
+    }
 
-	@RequestMapping(path = "taxcutting", method = RequestMethod.PUT)
-	public @ResponseBody ResponseDTO updateTaxCutting(@RequestBody TaxCuttingRule taxCuttingRule) {
-		RestTemplate restTemplate = RestTemplateBuilder.create();
-		restTemplate.put("cse://taxCutting/taxcutting",taxCuttingRule);
-		return success("编辑成功");
-	}
+    @RequestMapping(path = "taxcutting", method = RequestMethod.PUT)
+    public @ResponseBody ResponseDTO updateTaxCutting(@RequestBody TaxCuttingRule taxCuttingRule) {
+        RestTemplate restTemplate = RestTemplateBuilder.create();
+        restTemplate.put("cse://taxCutting/taxcutting", taxCuttingRule);
+        return success("编辑成功");
+    }
 
-	@RequestMapping(path = "taxcutting/{id}", method = RequestMethod.DELETE)
-	public @ResponseBody ResponseDTO deleteTaxCutting(@PathVariable("id") String id) {
-		RestTemplate restTemplate = RestTemplateBuilder.create();
-		restTemplate.delete("cse://taxCutting/taxcutting/"+id,id);
-		return success("删除成功");
-	}
+    @RequestMapping(path = "taxcutting/{id}", method = RequestMethod.DELETE)
+    public @ResponseBody ResponseDTO deleteTaxCutting(@PathVariable("id") String id) {
+        RestTemplate restTemplate = RestTemplateBuilder.create();
+        restTemplate.delete("cse://taxCutting/taxcutting/" + id, id);
+        return success("删除成功");
+    }
 
-	@SuppressWarnings("unchecked")
-	@RequestMapping(path = "taxcutting", method = RequestMethod.GET)
-	public @ResponseBody ResponseDTO listTaxCutting() {
-		RestTemplate restTemplate = RestTemplateBuilder.create();
-		List<TaxCuttingRule> taxCuttingRuleList = restTemplate.getForObject("cse://taxCutting/taxcutting",List.class);
-		return success("",taxCuttingRuleList,taxCuttingRuleList.size());
-	}
+    @SuppressWarnings("unchecked")
+    @RequestMapping(path = "taxcutting", method = RequestMethod.GET)
+    public @ResponseBody ResponseDTO listTaxCutting() {
+        RestTemplate restTemplate = RestTemplateBuilder.create();
+        List<TaxCuttingRule> taxCuttingRuleList = restTemplate.getForObject("cse://taxCutting/taxcutting", List.class);
+        return success("", taxCuttingRuleList, taxCuttingRuleList.size());
+    }
 
-	@RequestMapping(path = "taxcutting/{id}", method = RequestMethod.GET)
-	public @ResponseBody ResponseDTO getTaxCutting(@PathVariable("id") String id) {
-		RestTemplate restTemplate = RestTemplateBuilder.create();
-		TaxCuttingRule taxCuttingRule = restTemplate.getForObject("cse://taxCutting/taxcutting/"+id,TaxCuttingRule.class);
-		return success("",taxCuttingRule);
-	}
+    @RequestMapping(path = "taxcutting/{id}", method = RequestMethod.GET)
+    public @ResponseBody ResponseDTO getTaxCutting(@PathVariable("id") String id) {
+        RestTemplate restTemplate = RestTemplateBuilder.create();
+        TaxCuttingRule taxCuttingRule = restTemplate.getForObject("cse://taxCutting/taxcutting/" + id, TaxCuttingRule.class);
+        return success("", taxCuttingRule);
+    }
 
-	@SuppressWarnings("unchecked")
-	@RequestMapping(path = "taxcutting/largetypes/{codetype}", method = RequestMethod.GET)
-	public @ResponseBody ResponseDTO getLargeType(@PathVariable("codetype") String codetype) {
-		RestTemplate restTemplate = RestTemplateBuilder.create();
-		List<TaxCuttingCode> taxCuttingCodes = restTemplate.getForObject("cse://taxCutting/taxcutting/largetypes/"+codetype,List.class);
-		return success("",taxCuttingCodes);
-	}
-
-	@SuppressWarnings("unchecked")
-	@RequestMapping(path = "taxcutting/smalltypes/{codetype}", method = RequestMethod.GET)
-	public @ResponseBody ResponseDTO getSmallType(@PathVariable("codetype") String codetype) {
-		RestTemplate restTemplate = RestTemplateBuilder.create();
-		List<TaxCuttingCode> taxCuttingCodes = restTemplate.getForObject("cse://taxCutting/taxcutting/smalltypes/"+codetype,List.class);
-		return success("",taxCuttingCodes);
-	}
-
-	@SuppressWarnings("unchecked")
-	@RequestMapping(path = "taxcutting/ways/{codetype}", method = RequestMethod.GET)
-	public @ResponseBody ResponseDTO getWay(@PathVariable("codetype") String codetype) {
-		RestTemplate restTemplate = RestTemplateBuilder.create();
-		List<TaxCuttingCode> taxCuttingCodes = restTemplate.getForObject("cse://taxCutting/taxcutting/ways/"+codetype,List.class);
-		return success("",taxCuttingCodes);
-	}
+    // @SuppressWarnings("unchecked")
+    // @RequestMapping(path = "taxcutting/largetypes/{codetype}", method = RequestMethod.GET)
+    // public @ResponseBody ResponseDTO getLargeType(@PathVariable("codetype") String codetype) {
+    // RestTemplate restTemplate = RestTemplateBuilder.create();
+    // List<TaxCuttingCode> taxCuttingCodes = restTemplate.getForObject("cse://taxCutting/taxcutting/largetypes/"+codetype,List.class);
+    // return success("",taxCuttingCodes);
+    // }
+    //
+    // @SuppressWarnings("unchecked")
+    // @RequestMapping(path = "taxcutting/smalltypes/{codetype}", method = RequestMethod.GET)
+    // public @ResponseBody ResponseDTO getSmallType(@PathVariable("codetype") String codetype) {
+    // RestTemplate restTemplate = RestTemplateBuilder.create();
+    // List<TaxCuttingCode> taxCuttingCodes = restTemplate.getForObject("cse://taxCutting/taxcutting/smalltypes/"+codetype,List.class);
+    // return success("",taxCuttingCodes);
+    // }
+    //
+    // @SuppressWarnings("unchecked")
+    // @RequestMapping(path = "taxcutting/ways/{codetype}", method = RequestMethod.GET)
+    // public @ResponseBody ResponseDTO getWay(@PathVariable("codetype") String codetype) {
+    // RestTemplate restTemplate = RestTemplateBuilder.create();
+    // List<TaxCuttingCode> taxCuttingCodes = restTemplate.getForObject("cse://taxCutting/taxcutting/ways/"+codetype,List.class);
+    // return success("",taxCuttingCodes);
+    // }
 }
--- taxCutting/src/main/java/com/zving/declarationform/taxcutting/schema/TaxCuttingService.java
+++ taxCutting/src/main/java/com/zving/declarationform/taxcutting/schema/TaxCuttingService.java
@@ -2,7 +2,6 @@ package com.zving.declarationform.taxcutting.schema;
 
 import java.util.List;
 
-import com.zving.declarationform.model.TaxCuttingCode;
 import com.zving.declarationform.model.TaxCuttingRule;
 
 /**
@@ -16,41 +15,41 @@ import com.zving.declarationform.model.TaxCuttingRule;
  * @date 2017年10月20日
  */
 public interface TaxCuttingService {
-	/**
-	 * 添加减免税
-	 */
-	String addTaxCutting(TaxCuttingRule taxCuttingRule);
+    /**
+     * 添加减免税
+     */
+    String addTaxCutting(TaxCuttingRule taxCuttingRule);
 
-	/**
-	 * 修改减免税
-	 */
-	String updateTaxCutting(TaxCuttingRule taxCuttingRule);
+    /**
+     * 修改减免税
+     */
+    String updateTaxCutting(TaxCuttingRule taxCuttingRule);
 
-	/**
-	 * 删除减免税
-	 */
-	String deleteTaxCutting(String id);
+    /**
+     * 删除减免税
+     */
+    String deleteTaxCutting(String id);
 
-	/**
-	 * 获取所有减免税
-	 */
-	List<TaxCuttingRule> listTaxCutting();
+    /**
+     * 获取所有减免税
+     */
+    List<TaxCuttingRule> listTaxCutting();
 
-	/**
-	 * 获取单个减免税
-	 */
-	TaxCuttingRule getTaxCutting(String id);
+    /**
+     * 获取单个减免税
+     */
+    TaxCuttingRule getTaxCutting(String id);
 
-	/**
-	 * 获取减免税大类
-	 */
-	List<TaxCuttingCode> getLargeType(String codeType);
-	/**
-	 * 获取减免税小类
-	 */
-	List<TaxCuttingCode> getSmallType(String codeType);
-	/**
-	 * 获取减免税方式
-	 */
-	List<TaxCuttingCode> getWay(String codeType);
+    /**
+     * 获取减免税大类
+     */
+    // List<TaxCuttingCode> getLargeType(String codeType);
+    /**
+     * 获取减免税小类
+     */
+    // List<TaxCuttingCode> getSmallType(String codeType);
+    /**
+     * 获取减免税方式
+     */
+    // List<TaxCuttingCode> getWay(String codeType);
 }
