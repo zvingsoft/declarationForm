ab7026075857951a4a158bbad5fb2eb03c576d47
zhaochangjin <zhaochangjin@zving.com>
2017-10-23 17:33:55 +0800

    接口请求及数据展示调整

--- mainUI/source/views/setting/api/processingTradeAPI.js
+++ mainUI/source/views/setting/api/processingTradeAPI.js
@@ -1,21 +1,21 @@
 const processingTradeAPI = {
   getProcessingTradeList (pageIndex, pageSize) {
     return axios
-      .get(`/api/processingtrade/${pageIndex}/${pageSize}`)
+      .get(`/processingTrade/processingtrade`)
       .then(res => res.data)
   },
   getGoodsList () {
-    return axios.get(`/api/goodslist`).then(res => res.data)
+    return axios.get(`/processingTrade/goodslist`).then(res => res.data)
   },
   addFormData (model) {
-    return axios.post(`/api/processingtrade`, model).then(res => res.data)
+    return axios.post(`/processingTrade/processingtrade`, model).then(res => res.data)
   },
   editFormData (model) {
-    return axios.put(`/api/processingtrade`, model).then(res => res.data)
+    return axios.put(`/processingTrade/processingtrade`, model).then(res => res.data)
   },
   deleteDataList (ids) {
     let strIds = ids.join(',')
-    return axios.delete(`/api/processingtrade/${strIds}`).then(res => res.data)
+    return axios.delete(`/processingTrade/processingtrade/${strIds}`).then(res => res.data)
   }
 }
 
