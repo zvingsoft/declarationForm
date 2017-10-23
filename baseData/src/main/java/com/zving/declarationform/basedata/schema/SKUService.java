73f5ed71813efd02b6f5ab788805573ce7ac2ad5
xujunhao <xujunhao@zving.com>
2017-10-23 17:34:28 +0800

    完成sku商品管理页面

---
+++ baseData/src/main/java/com/zving/declarationform/basedata/schema/SKUService.java
@@ -0,0 +1,23 @@
+package com.zving.declarationform.basedata.schema;
+
+import java.util.List;
+
+import com.zving.declarationform.model.SKU;
+
+/**
+ * @author xujunhao
+ * @mail xujunhao@zving.com
+ * @date 2017年10月23日 
+ * @description:SKU相关服务
+ */
+public interface SKUService {
+    List<SKU> list(String searchWord);
+
+    SKU get(long id);
+
+    String delete(String ids);
+
+    String add(SKU sku);
+
+    String update(SKU sku);
+}
--- common/src/main/java/com/zving/declarationform/model/SKU.java
+++ common/src/main/java/com/zving/declarationform/model/SKU.java
@@ -8,17 +8,27 @@ package com.zving.declarationform.model;
  * @date 2017年10月17日
  */
 public class SKU {
-	String SN;// 编号
+	long id;
+	String sn;// 编号
 	String goodsType;// 商品类型
 	String name;// 商品名称
 	String spec;// 商品规格
 	String unit;// 商品单位
-	public String getSN() {
-		return SN;
+
+	public long getId() {
+		return id;
 	}
 
-	public void setSN(String sN) {
-		SN = sN;
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getSn() {
+		return sn;
+	}
+
+	public void setSn(String sn) {
+		this.sn = sn;
 	}
 
 	public String getGoodsType() {
@@ -52,5 +62,5 @@ public class SKU {
 	public void setUnit(String unit) {
 		this.unit = unit;
 	}
-	
+
 }
