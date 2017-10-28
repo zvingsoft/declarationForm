994bdaf5e49b7d3452ef220373f4b9d4aa15f335
Wang Yuchun <wyuch@zving.com>
2017-10-28 17:02:40 +0800

    docker build脚本完善

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 images=("baseData" "cottonQuota" "form" "license" "mainui" "manifest" "processingTrade" "riskAnalysis" "tax" "taxCutting")
 for var in ${images[@]};  
 do  
-		typeset -l lowerVar
-		lowerVar = $var
+    typeset -l lowerVar
+    lowerVar=$var
+    echo $lowerVar
     docker build -t $lowerVar $var
 done
--- push.sh
+++ push.sh
@@ -3,8 +3,8 @@
 images=("baseData" "cottonQuota" "form" "license" "mainui" "manifest" "processingTrade" "riskAnalysis" "tax" "taxCutting")
 for var in ${images[@]};  
 do  
-		typeset -l lowerVar
-		lowerVar = $var
+    typeset -l lowerVar
+    lowerVar=$var
     docker tag $lowerVar "$1/$lowerVar"
     docker push "$1/$lowerVar"
 done
